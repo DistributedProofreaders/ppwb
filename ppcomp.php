4618cafac100b5284c27e31c9d719275799ff3b1
Roger Frank <rfrank@rfrank.net>
2019-04-03 23:50:17 +0000

    wording on ppcomp writeup

--- ppcomp.php
+++ ppcomp.php
@@ -36,7 +36,7 @@ the results.</p>
 <p>Note: The ppcomp program program was originally written as the standalone program comp_pp.py
 by bibimbop at pgdp. It is used as part of the DP Workbench with permission.
 Users who want a compare tool with many more options might find
-the <a href='https://pptools.tangledhelix.com'>tangledhelix</a> site useful.
+bibimbop's <a href='https://pptools.tangledhelix.com'>tangledhelix</a> site useful.
 </p>
 MENU;
 }
