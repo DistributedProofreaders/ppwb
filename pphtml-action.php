24fb1760848fe395e03e805336db87f96a3e61f7
Casey Peel <cpeel@users.noreply.github.com>
2020-12-04 06:14:37 +0000

    Centralize upload logic
    Move file upload and validation logic into base.inc. As part of this,
    restrict uploaded filenames to only use a limited set of characters.
    This will help avoid problems when passing the files to the scripts.

--- pphtml-action.php
+++ pphtml-action.php
@@ -1,75 +1,20 @@
 <?php
 require_once("base.inc");
 
-$errors = array(); // place to save error messages
 $work = "t"; // a working folder for project data
 $upid = uniqid('r'); // unique project id
 $extensions = array( // allowed file extensions
     "zip"
 );
-$target_name = "";
-$gtarget_name = "";
 
 // create a unique workbench project folder in t
-mkdir($work . "/" . $upid, 0755);
-
+$workdir = "$work/$upid";
+mkdir($workdir, 0755);
 
 // ----- process the main project file ---------------------------------
 
-if (isset($_FILES['userfile']) && $_FILES['userfile']['name'] != "") {
+$target_name = process_file_upload("userfile", $workdir, $extensions);
 
-    // get the information about the file
-    $file_name = $_FILES['userfile']['name'];
-    $file_size = $_FILES['userfile']['size'];
-    $file_tmp = $_FILES['userfile']['tmp_name'];
-    $file_type = $_FILES['userfile']['type'];
-    $file_ext = pathinfo($file_name, PATHINFO_EXTENSION);
-    move_uploaded_file($file_tmp, "/tmp/" . $upid);
-
-    // begin a series of validation tests
-
-    // does it pass the anti-virus tests?
-    $av_test_result = array();
-    $av_retval = 0;
-    $cmd = "/usr/bin/clamdscan " . escapeshellcmd("/tmp/" . $upid);
-    exec($cmd, $av_test_result, $av_retval);
-    if ($av_retval == 1) {
-        $errors[] = "input file rejected by clamdscan";
-        // destroy uploaded file
-        unlink( escapeshellcmd("/tmp/" . $upid) );
-    }
-
-    // was a file uploaded?
-    if ($_FILES['userfile']['size'] == 0) {
-        $errors[] = 'no file was uploaded';
-    }
-
-    // do they claim it's an allowed type?
-    if (in_array($file_ext, $extensions) === false) {
-        $errors[] = "please upload a .zip file";
-    }
-
-    // is it small enough?
-    if ($file_size > 40000000) {
-        $errors[] = "file size must be less than 40 MB";
-    }
-
-    // if any errors, report and terminate
-    if (empty($errors) == false) {
-        echo "<pre>";
-        echo "process terminated during processing uploaded file:<br/>";
-        foreach ($errors as $key => $value) {
-            echo $value . "<br/>";
-        }
-        echo "</pre>";
-        exit(1);
-    }
-
-    // move the uploaded file to the project folder
-    $target_name = $work . "/" . $upid . "/" . $file_name;
-    rename("/tmp/" . $upid, $target_name);
-}
-  
 // ----- no errors. proceed ----------------------------------------
 
 log_tool_access("pphtml", $upid);
