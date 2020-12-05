0d32cfcfbd5cbfce99daeeae8016269866db90b4
Casey Peel <cpeel@users.noreply.github.com>
2020-12-05 04:18:28 +0000

    Update maintenance script
    Update the maintenance script to use the configuration file. Using
    'rm -rf' instead of iterating over the filesystem is marginally
    safer since it takes care of things like symlinks and other oddities.

--- maint.php
+++ maint.php
@@ -1,29 +1,63 @@
 <?php
+require_once("base.inc");
 
-$action = 1;
+// Maintenance script to remove files older files. This script should be run
+// via a crontab *through the web server* to ensure it has the correct
+// permissions, eg:
+//     URL=https://www.pgdp.org/ppwb/maint.php;
+//     /usr/bin/wget --quiet --tries=1 --timeout=0 -O- $URL
+// This script ensures that only runs from localhost will succeed.
 
-/*
-set $action = 1 to run this code
-clear out the "t" working directory contents
-make sure it is the right $dir
-when done, make a new 't', chmod 0777 and create a file access.log 0777
-*/
-if ($action == 1) {
-	$dir = '/home/rfrank/public_html/dev/t';
-	$it = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
-	$files = new RecursiveIteratorIterator($it,
-	             RecursiveIteratorIterator::CHILD_FIRST);
-	foreach($files as $file) {
-	    if ($file->isDir()){
-	        rmdir($file->getRealPath());
-	    } else {
-	        unlink($file->getRealPath());
-	    }
-	}
-	// rmdir($dir);  leave the parent directory intact
-	echo "done";
+// Reject calls not by localhost or other authorized callers
+if(!requester_is_authorized()) {
+    die("You are not authorized to perform this request.\n");
 }
 
-/* if I need to remove specific files owned by www-data: */
-// unlink("/home/rfrank/public_html/test/t/command.txt");
-// unlink("/home/rfrank/public_html/test/t/output.txt");
+// Walk through our workdir deleting directories that are older than
+// $maint_days_ago
+$maint_cutoff = time() - ($maint_days_ago * 60*60*24);
+foreach(new DirectoryIterator($base_workdir) as $dir) {
+    // skip any dot files or access.log
+    if($dir->isDot() || $dir->getFilename() == "access.log") {
+        continue;
+    }
+
+    if($dir->getMTime() < $maint_cutoff) {
+        echo "Deleting " . $dir->getFilename() . "\n";
+
+        $scommand = join(" ", [
+            "rm",
+            "-rf",
+            escapeshellarg("$base_workdir/" . $dir->getFilename())
+        ]);
+        $command = escapeshellcmd($scommand);
+
+        // echo "$command\n";
+        shell_exec($command);
+    }
+}
+
+//---------------------------------------------------------------------------
+
+// function to determine if page requester is from localhost or in our OK list
+function requester_is_authorized()
+{
+    global $maint_ips;
+
+    if(php_sapi_name() == "cli")
+    {
+        return FALSE;
+    }
+
+    if($_SERVER['REMOTE_ADDR'] == '127.0.0.1' ||
+        $_SERVER['REMOTE_ADDR'] == $_SERVER['SERVER_ADDR'] ||
+        in_array($_SERVER['REMOTE_ADDR'], $maint_ips))
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
