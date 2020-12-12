5e0e4257a8b7b09e75c4440f4f7a6b4b6965a06c
Casey Peel <cpeel@users.noreply.github.com>
2020-12-12 04:19:06 +0000

    Don't output pptext version on input page

--- pptext.php
+++ pptext.php
@@ -106,9 +106,6 @@ Left click to view or right click the link to download the results.</p>
 
 </form>
 MENU;
-$command = escapeshellcmd('bin/pptext -r');
-$output = shell_exec($command);
-echo "<div style='text-align:right; font-size:70%; color:white;'>pptext version: ".$output."</div>";
 }
 
 function get_js()
