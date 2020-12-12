22e97765d9a5133d8067ab3a31b9dd8276dbfa4b
Casey Peel <cpeel@users.noreply.github.com>
2020-12-12 04:35:25 +0000

    Log tool output to a single file in $workdir
    For each tool run, log information about the run into a single file
    in $workdir. This simplifies debugging and troubleshooting.

--- pphtml-action.php
+++ pphtml-action.php
@@ -64,11 +64,12 @@ $command = join(" ", [
     "2>&1"
 ]);
 
-// echo $command;
-file_put_contents("$workdir/command.txt", $command);
+log_tool_action($workdir, "command", $command);
 
 // and finally, run pphtml
 $output = shell_exec($command);
+
+log_tool_action($workdir, "output", $output);
 
 // ----- display results -------------------------------------------
 
@@ -89,4 +90,3 @@ if ($reportok) {
     <tt>${output}</tt></p>
     </p>For more assistance, ask in the <a href='$help_url'>discussion topic</a> and include this identifier: ${upid}</p>";
 }
-
