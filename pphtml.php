cd9ae79cc2ae96cad77f61cfb3204f315ba498d7
okrick <okrick@gmail.com>
2021-05-07 13:16:31 -0700

    Update pphtml.php (#16)
    Describe a rarely occurring problem with pphtml.

--- pphtml.php
+++ pphtml.php
@@ -19,6 +19,10 @@ click Submit to run pphtml against your uploaded zip. When it's
 finished you should see a screen announcing "Pphtml Results" with a link to the results
 of the run. Left click to view or right click the link to download the results.</p>
 
+<p>Please note that HTML a (link) tags must begin and end on a single line for this
+to work—the beginning &lt; must be in the same row with the &gt;. Otherwise the link
+test will report these as [FAIL] duplicate targets.</p>
+
 <form target="_blank" action="pphtml-action.php" method="POST" enctype="multipart/form-data">
 
 <input type="checkbox" name="ver" value="Yes" id="ver" autocomplete="off">
