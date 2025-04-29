8a8578986760c21a1142a34709c5f6baf5a13756
Ross Olson <rolson@gmail.com>
2025-04-29 14:39:45 -0700

    Update techinfo-pphtml.php - typo (#40)
    'pptext' to 'pphtml' in documentation.

--- techinfo-pphtml.php
+++ techinfo-pphtml.php
@@ -6,7 +6,7 @@ output_header("pphtml", ["pphtml.php" => "PPHTML"]);
 
 <p>Technical info for pphtml.</p>
 <ol>
-<li>There is a valid CSS construction of <tt style='border: 1px solid silver; background-color:#FFFFCC'>.class1.class2</tt> that pptext does
+<li>There is a valid CSS construction of <tt style='border: 1px solid silver; background-color:#FFFFCC'>.class1.class2</tt> that pphtml does
 not handle correctly. That definition means "an element with both class1 and class2" as in
 <tt style='border: 1px solid silver; background-color:#FFFFCC'>&lt;hr class='class1 green class2' /></tt>.
 Currently, that construction will show up as "defined but not used".
